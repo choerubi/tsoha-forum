05375e7da332bb6671a02feb39d25be139bb1f1f
Porola Iida S M <poii@cubbli22-gold.local>
2024-05-05 21:41:30 +0300

    Update schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,33 @@ CREATE TABLE users (
     password TEXT,
     role INTEGER
 );
+
+CREATE TABLE forums (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users,
+    created_at TIMESTAMP,
+    visible INTEGER
+);
+
+CREATE TABLE threads (
+    id SERIAL PRIMARY KEY,
+    title TEXT,
+    user_id INTEGER REFERENCES users,
+    forum_id INTEGER REFERENCES forums,
+    created_at TIMESTAMP
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    thread_id INTEGER REFERENCES threads,
+    sent_at TIMESTAMP
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    message_id INTEGER REFERENCES messages
+);
